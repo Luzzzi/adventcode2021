9773aaea32fc7f3f587048110a971eb06a6e3977
Luzzzi <lefrapperl@gmail.com>
2021-12-06 20:10:40 +0100

    Day 6 part 1 optimized

--- 06/06.py
+++ 06/06.py
@@ -1,23 +1,22 @@
-with open("input.txt", "r") as fd:
+with open("input2.txt", "r") as fd:
     lines = fd.readlines()
     lines = [line.rstrip() for line in lines]
 
 
-initial_state = lines[0].replace(",", "")
+initial_state = lines[0].split(",")
 initial_state = [int(i) for i in initial_state]
-days = 0
 fish = 0
+days = 80
 
-while days < 80: 
-    days += 1
-    print("After", days -1, "day")
-    print(initial_state)
+for day in range(days):
     for i in range(len(initial_state)):
-        if initial_state[i] <= 8 and initial_state[i] > 0: 
+        if initial_state[i] > 0: 
             initial_state[i] -= 1
-        elif initial_state[i] == 0: 
+        else:
             initial_state[i] += 6
             initial_state.append(8)
+    print(day -1)
+
 
 fish = len(initial_state)
 print(fish)
